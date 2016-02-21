6836a0358dd7110c81bd5899382d320ccceb405f
Davide Steduto <dave.dna@gmail.com>
2016-02-21 22:00:29 +0100

    Added Copyright notice on the top of the files

--- flexible-adapter/src/main/java/eu/davidea/flexibleadapter/items/IExpandable.java
+++ flexible-adapter/src/main/java/eu/davidea/flexibleadapter/items/IExpandable.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Davide Steduto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.davidea.flexibleadapter.items;
 
 import android.support.v7.widget.RecyclerView;
@@ -7,7 +22,7 @@ import java.util.List;
 /**
  * Interface to manage expanding operations on items with
  * {@link eu.davidea.flexibleadapter.FlexibleAdapter}.
-  * <p>Implements this interface or use {@link AbstractExpandableItem}.</p>
+ * <p>Implements this interface or use {@link AbstractExpandableItem}.</p>
  *
  * @author Davide Steduto
  * @since 17/01/2016 Created
