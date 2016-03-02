a3764163d9f93ca1681ca1f7d52049e757977fda
Martin Guillon <martin.guillon@akylas.fr>
2016-03-02 18:42:00 +0100

    divider height

--- flexible-adapter/src/main/java/eu/davidea/flexibleadapter/common/DividerItemDecoration.java
+++ flexible-adapter/src/main/java/eu/davidea/flexibleadapter/common/DividerItemDecoration.java
@@ -3,14 +3,17 @@ package eu.davidea.flexibleadapter.common;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import eu.davidea.flexibleadapter.section.HeaderViewHolder;
 
 public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 	private Drawable mDivider;
+	private int mDividerHeight;
 	
 	private static final int[] ATTRS = new int[]{
 		android.R.attr.listDivider
@@ -22,6 +25,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 	public DividerItemDecoration(Context context) {
 		final TypedArray styledAttributes = context.obtainStyledAttributes(ATTRS);
 		mDivider = styledAttributes.getDrawable(0);
+		mDividerHeight = mDivider.getIntrinsicHeight();
 		styledAttributes.recycle();
 	}
 
@@ -30,9 +34,25 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 	 */
 	public DividerItemDecoration(Context context, int resId) {
 		mDivider = ContextCompat.getDrawable(context, resId);
+        mDividerHeight = mDivider.getIntrinsicHeight();
 	}
-
- 
+	
+	public Drawable getDivider() {
+	    return mDivider;
+	}
+	
+	public void setDividerHeight(int height) {
+        mDividerHeight = height;
+    }
+	
+	@Override
+	public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+	    RecyclerView.ViewHolder holder = parent.getChildViewHolder(view);
+	    if (!(holder instanceof HeaderViewHolder)) {
+	        outRect.bottom = mDividerHeight;
+	    }
+	}
+	
 	@Override
 	public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
 		int left = parent.getPaddingLeft();
@@ -41,12 +61,15 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 		int childCount = parent.getChildCount();
 		for (int i = 0; i < childCount; i++) {
 			View child = parent.getChildAt(i);
- 
+			RecyclerView.ViewHolder holder = parent.getChildViewHolder(child);
+	        if (holder instanceof HeaderViewHolder) {
+	            continue;
+	        }
 			RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
  
 			int top = child.getBottom() + params.bottomMargin +
 					Math.round(ViewCompat.getTranslationY(child));
-			int bottom = top + mDivider.getIntrinsicHeight();
+			int bottom = top + mDividerHeight;
  
 			mDivider.setBounds(left, top, right, bottom);
 			mDivider.draw(c);
