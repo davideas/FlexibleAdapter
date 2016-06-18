79e504a713ccc74c5a944cfb1b25f979fcb3dc6e
Davide Steduto <dave.dna@gmail.com>
2016-06-18 17:03:25 +0200

    Cleaned FlexibleViewHolder

--- flexible-adapter/src/main/java/eu/davidea/viewholders/FlexibleViewHolder.java
+++ flexible-adapter/src/main/java/eu/davidea/viewholders/FlexibleViewHolder.java
@@ -19,12 +19,10 @@ import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
 
 import eu.davidea.flexibleadapter.FlexibleAdapter;
 import eu.davidea.flexibleadapter.SelectableAdapter;
@@ -344,66 +342,6 @@ public abstract class FlexibleViewHolder extends ContentViewHolder
 	@Override
 	public View getRearRightView() {
 		return null;
-	}
-
-	private static class ContentViewHolder extends RecyclerView.ViewHolder {
-
-		private int mBackupPosition = RecyclerView.NO_POSITION;
-		private View contentView;
-
-		public ContentViewHolder(View view, FlexibleAdapter adapter, boolean stickyHeader) {
-			//Since itemView is declared "final", the split is done before the View is initialized
-			super(stickyHeader ? new FrameLayout(view.getContext()) : view);
-
-			if (stickyHeader) {
-				itemView.setLayoutParams(adapter.getRecyclerView().getLayoutManager()
-						.generateLayoutParams(view.getLayoutParams()));
-				((FrameLayout) itemView).addView(view);//Add View after setLayoutParams
-				contentView = view;
-			}
-		}
-
-	/*-----------------------*/
-	/* STICKY HEADER METHODS */
-	/*-----------------------*/
-
-		/**
-		 * In case this ViewHolder represents a Header Item, this method returns the contentView of the
-		 * FrameLayout, otherwise it returns the basic itemView.
-		 *
-		 * @return the real contentView
-		 */
-		public View getContentView() {
-			return contentView != null ? contentView : itemView;
-		}
-
-		/**
-		 * Overcomes the situation of returning an unknown position (-1) of ViewHolders created out of
-		 * the LayoutManager (ex. StickyHeaders).
-		 * <p><b>NOTE:</b> Always call this method, instead of {@code getAdapterPosition()}, in case
-		 * of StickyHeaders use case.</p>
-		 *
-		 * @return the Adapter position result of {@link #getAdapterPosition()} OR the backup position
-		 * preset and known, if the previous result was {@link RecyclerView#NO_POSITION}.
-		 * @see #setBackupPosition(int)
-		 */
-		public int getFlexibleAdapterPosition() {
-			int position = getAdapterPosition();
-			if (position == RecyclerView.NO_POSITION) {
-				position = mBackupPosition;
-			}
-			return position;
-		}
-
-		/**
-		 * Restore the Adapter position if the original Adapter position is unknown.
-		 * <p>Called by StickyHeaderHelper to support the clickListeners events.</p>
-		 *
-		 * @param backupPosition the known position of this ViewHolder
-		 */
-		public void setBackupPosition(int backupPosition) {
-			mBackupPosition = backupPosition;
-		}
 	}
 
 }
