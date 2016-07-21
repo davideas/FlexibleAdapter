977ba64d0340c7f3feef89d589dc865b609dde2a
Davide Steduto <dave.dna@gmail.com>
2016-07-21 19:25:19 +0200

    More JUnit test for HeadersSectionsTest

--- flexible-adapter-app/src/test/java/eu/davidea/flexibleadapter/HeadersSectionsTest.java
+++ flexible-adapter-app/src/test/java/eu/davidea/flexibleadapter/HeadersSectionsTest.java
@@ -9,8 +9,11 @@ import org.robolectric.annotation.Config;
 import java.util.List;
 
 import eu.davidea.flexibleadapter.items.AbstractFlexibleItem;
+import eu.davidea.flexibleadapter.items.IHeader;
 import eu.davidea.samples.flexibleadapter.services.DatabaseService;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -28,51 +31,91 @@ public class HeadersSectionsTest {
 	public void setUp() throws Exception {
 		DatabaseService.getInstance().createHeadersSectionsDatabase(30, 5);
 		mItems = DatabaseService.getInstance().getDatabaseList();
-		mAdapter = new FlexibleAdapter<>(mItems);
 	}
 
 	@Test
 	public void testItemCount() throws Exception {
-		assertTrue(mAdapter.getItemCount() == mItems.size());
+		mAdapter = new FlexibleAdapter<>(mItems);
+		assertEquals(mItems.size(), mAdapter.getItemCount());
 	}
 
 	@Test
 	public void testSetDisplayHeadersAtStartUp() throws Exception {
-		assertTrue(mAdapter.getHeaderItems().size() == 0);
+		mAdapter = new FlexibleAdapter<>(mItems);
+		assertEquals(0, mAdapter.getHeaderItems().size());
+
+		//1st call to display headers
 		mAdapter.setDisplayHeadersAtStartUp(true);
-		System.out.println("Headers = " + mAdapter.getHeaderItems().size());
+		System.out.println("1st call Headers = " + mAdapter.getHeaderItems().size());
+		assertEquals(5, mAdapter.getHeaderItems().size());
+
+		//2nd call to display headers
 		mAdapter.setDisplayHeadersAtStartUp(true);
-		System.out.println("Headers = " + mAdapter.getHeaderItems().size());
-		assertTrue(mAdapter.getHeaderItems().size() > 0);
+		System.out.println("2nd call Headers = " + mAdapter.getHeaderItems().size());
+		assertEquals(5, mAdapter.getHeaderItems().size());
+
+		//3rd call to display headers
+		mAdapter.showAllHeaders();
+		System.out.println("3rd call Headers = " + mAdapter.getHeaderItems().size());
+		assertEquals(5, mAdapter.getHeaderItems().size());
+	}
+
+	@Test
+	public void testEmptyAdapterAddItemsWithHeader() throws Exception {
+		mAdapter = new FlexibleAdapter<>(null);
+
+		mAdapter.addItems(0, mItems);
+		assertEquals(mItems.size(), mAdapter.getItemCount());
+
+		mAdapter.setDisplayHeadersAtStartUp(true);
+		assertEquals(5, mAdapter.getHeaderItems().size());
 	}
 
 	@Test
 	public void testIsHeader() throws Exception {
+		mAdapter = new FlexibleAdapter<>(mItems);
+		mAdapter.setDisplayHeadersAtStartUp(true);
+		AbstractFlexibleItem item = mAdapter.getItem(0);
+		assertNotNull(item);
+		assertTrue(mAdapter.isHeader(item));
+
 	}
 
 	@Test
 	public void testHasHeader() throws Exception {
-
+		mAdapter = new FlexibleAdapter<>(mItems);
+		mAdapter.setDisplayHeadersAtStartUp(true);
+		AbstractFlexibleItem item = mAdapter.getItem(1);
+		assertNotNull(item);
+		assertTrue(mAdapter.hasHeader(item));
 	}
 
 	@Test
 	public void testHasSameHeader() throws Exception {
-
+		mAdapter = new FlexibleAdapter<>(mItems);
+		mAdapter.setDisplayHeadersAtStartUp(true);
+		AbstractFlexibleItem item1 = mAdapter.getItem(1);
+		IHeader header = mAdapter.getHeaderOf(item1);
+		assertNotNull(header);
+		AbstractFlexibleItem item2 = mAdapter.getItem(3);
+		assertNotNull(item2);
+		assertTrue(mAdapter.hasSameHeader(item2, header));
 	}
 
 	@Test
 	public void testGetHeaderOf() throws Exception {
-
+		mAdapter = new FlexibleAdapter<>(mItems);
+		IHeader header = mAdapter.getHeaderOf(mItems.get(0));
+		assertNotNull(header);
 	}
 
 	@Test
-	public void testShowAllHeaders() throws Exception {
-
-	}
-
-	@Test
-	public void testHideAllHeaders() throws Exception {
-
+	public void testShowAndHideAllHeaders() throws Exception {
+		mAdapter = new FlexibleAdapter<>(mItems);
+		mAdapter.showAllHeaders();
+		assertEquals(5, mAdapter.getHeaderItems().size());
+		mAdapter.hideAllHeaders();
+		assertEquals(0, mAdapter.getHeaderItems().size());
 	}
 
 }
