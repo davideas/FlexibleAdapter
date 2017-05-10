8a748c56d274e5c92cd21796433243e4322d3ffb
Stepan Sanda <stepan.sanda@gmail.com>
2017-05-10 15:15:23 +0200

    Fix condition for showing header

--- flexible-adapter/src/main/java/eu/davidea/flexibleadapter/FlexibleAdapter.java
+++ flexible-adapter/src/main/java/eu/davidea/flexibleadapter/FlexibleAdapter.java
@@ -5503,7 +5503,7 @@ public class FlexibleAdapter<T extends IFlexible>
 				headersShown = true;
 			}
 			IHeader header = getHeaderOf(item);
-			if (header != null && header.equals(sameHeader) && !isExpandable((T) header)) {
+			if (header != null && !header.equals(sameHeader) && !isExpandable((T) header)) {
 				header.setHidden(false);
 				sameHeader = header;
 				newItems.add(position, (T) header);
