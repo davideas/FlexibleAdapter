811c6f64fec2929ed2cea0b394b7e549be82f77c
Tom Koptel <artem.koptel@sap.com>
2019-06-10 10:06:26 +0200

    Add tab layout item

--- flexible-adapter-app/src/main/java/eu/davidea/samples/flexibleadapter/TravelRequestDemo.kt
+++ flexible-adapter-app/src/main/java/eu/davidea/samples/flexibleadapter/TravelRequestDemo.kt
@@ -2,9 +2,12 @@ package eu.davidea.samples.flexibleadapter
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TableLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.common.SmoothScrollLinearLayoutManager
 import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
@@ -23,7 +26,10 @@ class TravelRequestDemo : AppCompatActivity() {
             layoutManager = SmoothScrollLinearLayoutManager(context)
             adapter = exampleAdapter
         }
-        exampleAdapter.updateDataSet(listOf(RequestDetailsHeader(RequestDetailsModel("Hi"))))
+        exampleAdapter.updateDataSet(listOf(
+            RequestDetailsHeader(RequestDetailsModel("Here go details")),
+            RequestDetailsTab(RequestDetailsTabModel("Details", "Expenses"))
+        ))
     }
 }
 
@@ -61,7 +67,7 @@ class RequestDetailsHeader(
     }
 
     class RequestDetailsViewHolder(val view: View, adapter: FlexibleAdapter<*>) :
-        FlexibleViewHolder(view, adapter, true) {
+        FlexibleViewHolder(view, adapter, false) {
         val textView by lazy(LazyThreadSafetyMode.NONE) {
             view.findViewById<TextView>(R.id.tr_details_title)
         }
@@ -69,3 +75,58 @@ class RequestDetailsHeader(
 }
 
 data class RequestDetailsModel(val title: String)
+
+class RequestDetailsTab(
+    private val internalModel: RequestDetailsTabModel
+) : AbstractFlexibleItem<RequestDetailsTab.RequestDetailsTabViewHolder>(),
+    IHolder<RequestDetailsTabModel> {
+
+    override fun createViewHolder(
+        view: View,
+        adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>
+    ): RequestDetailsTabViewHolder {
+        return RequestDetailsTabViewHolder(view, adapter)
+    }
+
+    override fun bindViewHolder(
+        adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>,
+        holder: RequestDetailsTabViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        holder.tab1(model.title1)
+        holder.tab2(model.title2)
+    }
+
+    override fun getModel(): RequestDetailsTabModel = internalModel
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is RequestDetailsTabModel) {
+            other == internalModel
+        } else {
+            false
+        }
+    }
+
+    override fun getLayoutRes(): Int = R.layout.tr_request_tab
+
+    class RequestDetailsTabViewHolder(val view: View, adapter: FlexibleAdapter<*>) :
+        FlexibleViewHolder(view, adapter, true) {
+        private val tabLayout by lazy(LazyThreadSafetyMode.NONE) {
+            view.findViewById<TabLayout>(R.id.tabLayout)
+        }
+
+        fun tab1(text: String) {
+            tabLayout.getTabAt(0)?.text = text
+        }
+
+        fun tab2(text: String) {
+            tabLayout.getTabAt(1)?.text = text
+        }
+    }
+}
+
+data class RequestDetailsTabModel(
+    val title1: String,
+    val title2: String
+)
